7c0d6e5242b08a3e2f65b06fe02f3c680dc9c823
jromero <jonathan.joseromero@gmail.com>
2016-10-22 19:48:21 -0300

    modified the fields data store to match the one created at sequelize

--- client/extjs/app/view/Usuarios.js
+++ client/extjs/app/view/Usuarios.js
@@ -3,24 +3,19 @@ Ext.define('Demo.view.Usuarios',{
     title: 'Usuarios',
     xtype: 'Usuarios',
     store: {
-    fields:['name', 'email', 'phone'],
+    fields:['firstName','lastName'],
         data:[
-        { name: 'Lisa',  email: "lisa@simpsons.com",
-            phone: "555-111-1224"  },
-        { name: 'Bart',  email: "bart@simpsons.com",
-            phone: "555-222-1234" },
-        { name: 'Homer', email: "home@simpsons.com",
-            phone: "555-222-1244"  },
-        { name: 'Marge', email: "marge@simpsons.com",
-            phone: "555-222-1254"  }
+        { firstName: 'Lisa', lastName: "Simpsons"  },
+            { firstName: 'Bart', lastName: "Simpsons"  },
+            { firstName: 'Homer', lastName: "Simpsons"  },
+            { firstName: 'Maggie', lastName: "Simpsonite"  }
     ],
         proxy: {
         type: 'memory'
     }
 },
     columns: [
-        { text: 'Name',  dataIndex: 'name' },
-        { text: 'Email', dataIndex: 'email', flex: 1},
-        { text: 'Phone', dataIndex: 'phone' }
+        { text: 'First Name',  dataIndex: 'firstName' },
+        { text: 'Last Name', dataIndex: 'lastName' }
     ]
 });
