6505871c07036fbc75ec98bc8cb7f873a9cbf1ee
jromero <jonathan.joseromero@gmail.com>
2016-10-21 23:45:32 -0300

    added sequelize to the project and made a simple connection

---
+++ server/mysql/sequelize.js
@@ -0,0 +1,21 @@
+
+var Sequelize = require('sequelize');
+
+var sequelize = new Sequelize('Demo', 'root', 'root', {
+    host: 'localhost',
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    }
+});
+
+sequelize
+    .authenticate()
+    .then(function(err) {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(function (err) {
+        console.log('Unable to connect to the database:', err);
+    });
