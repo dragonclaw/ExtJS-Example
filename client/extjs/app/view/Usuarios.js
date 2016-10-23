d6ed65d02755fea11877a37452fbbc1855b40396
jromero <jonathan.joseromero@gmail.com>
2016-10-22 21:13:20 -0300

    added dbl-click functionalities

--- client/extjs/app/view/Usuarios.js
+++ client/extjs/app/view/Usuarios.js
@@ -2,6 +2,17 @@ Ext.define('Demo.view.Usuarios',{
     extend: 'Ext.tree.Panel',
     title: 'Usuarios',
     xtype: 'Usuarios',
+    listeners: {
+        dblclick : {
+            fn: function(dv, record, item, index, dataIndex) {
+                alert(dataIndex);
+            },
+            // You can also pass 'body' if you don't want click on the header or
+            // docked elements
+            element: 'body',
+            scope: this
+        }
+    },
     store: {
     fields:['firstName','lastName'],
         data:[
